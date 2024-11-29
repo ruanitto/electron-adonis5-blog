7626cfe0f66885ad59b1e5a09830196c76075525
Rafael Gomes <rafael.gomes@madsolutions.com.br>
2024-11-29 12:54:25 -0300

    update project nd fix files

--- config/app.ts
+++ config/app.ts
@@ -27,7 +27,7 @@ type HttpConfig = RequestConfig & ResponseConfig
 | be decrypted.
 |
 */
-export const appKey: string = Env.getOrFail('APP_KEY') as string
+export const appKey: string = Env.get('APP_KEY') as string
 
 /*
 |--------------------------------------------------------------------------
@@ -123,12 +123,12 @@ export const http: HttpConfig = {
   | However, it is a very common these days that API servers always wants to
   | make response in JSON regardless of the existence of the `Accept` header.
   |
-  | By setting `forceContentNegotiationToJSON = true`, you negotiate with the
-  | server in advance to always return JSON without relying on the client
-  | to set the header explicitly.
+  | By setting `forceContentNegotiationTo = 'application/json'`, you negotiate
+  | with the server in advance to always return JSON without relying on the
+  | client to set the header explicitly.
   |
   */
-  forceContentNegotiationToJSON: false,
+  // forceContentNegotiationTo: 'application/json',
 }
 
 /*
